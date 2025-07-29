fad69d0780996644828f1fac5af7dea4c8dddeb9
jasonSuiyt <565784544@qq.com>
2025-07-29 20:57:46 +0800

    build: 更新版本号至2.0.0并优化主键查询SQL
    优化主键查询SQL，添加indisprimary条件确保只查询主键索引，并移除不必要的字段

--- src/pg_meta.rs
+++ src/pg_meta.rs
@@ -71,21 +71,22 @@ WHERE c.relnamespace = n.oid and n.nspname = 'public' and c.relkind = 'r';";
     /// 设置表的主键信息
     async fn set_primary_key(&self, table_vec: &mut Vec<TableInfo>) -> Result<(), MetaError> {
         let sql = "SELECT result.TABLE_SCHEMA, result.TABLE_NAME, result.COLUMN_NAME, result.KEY_SEQ, result.PK_NAME
-FROM (SELECT NULL AS TABLE_CAT,
-             n.nspname AS TABLE_SCHEMA,
-             ct.relname AS TABLE_NAME,
-             a.attname AS COLUMN_NAME,
-             (information_schema._pg_expandarray(i.indkey)).n AS KEY_SEQ,
-             ci.relname AS PK_NAME,
-             information_schema._pg_expandarray(i.indkey) AS KEYS,
-             a.attnum AS A_ATTNUM
-      FROM pg_catalog.pg_class ct
-               JOIN pg_catalog.pg_attribute a ON (ct.oid = a.attrelid)
-               JOIN pg_catalog.pg_namespace n ON (ct.relnamespace = n.oid)
-               JOIN pg_catalog.pg_index i ON (a.attrelid = i.indrelid)
-               JOIN pg_catalog.pg_class ci ON (ci.oid = i.indexrelid)
-      WHERE ci.relname ~ '_pkey$'
-        AND a.attnum = ANY(i.indkey)) result
+FROM (
+    SELECT
+        n.nspname AS TABLE_SCHEMA,
+        ct.relname AS TABLE_NAME,
+        a.attname AS COLUMN_NAME,
+        (information_schema._pg_expandarray(i.indkey)).n AS KEY_SEQ,
+        ci.relname AS PK_NAME,
+        a.attnum AS A_ATTNUM
+    FROM pg_catalog.pg_class ct
+    JOIN pg_catalog.pg_attribute a ON (ct.oid = a.attrelid)
+    JOIN pg_catalog.pg_namespace n ON (ct.relnamespace = n.oid)
+    JOIN pg_catalog.pg_index i ON (a.attrelid = i.indrelid AND i.indisprimary = true)
+    JOIN pg_catalog.pg_class ci ON (ci.oid = i.indexrelid)
+    WHERE a.attnum = ANY(i.indkey)
+        AND n.nspname = 'public'
+) result
 ORDER BY result.table_name, result.pk_name, result.key_seq";
 
         let result = sqlx::query(sql).fetch_all(&self.pool).await?;
